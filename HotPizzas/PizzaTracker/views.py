5cef50f7388115c6bf87e6251e1a3b44554a1af7
David J. Felix <felix.davidj@gmail.com>
2014-05-03 17:42:59 -0400

    Add delivered pizzas and to deliver pizzas
    Repeat code a bunch, but DRY sometimes, but DRY sometimes

--- HotPizzas/PizzaTracker/views.py
+++ HotPizzas/PizzaTracker/views.py
@@ -10,19 +10,42 @@ def home(request):
 def available_pizzas(request):
 	pizzas = list()
 	for pizza in Pizza.objects.select_related().filter(delivered=False).filter(driver=request.user.id):
-		formatted_pizza = dict()
-		formatted_pizza["cook_time"] = str(pizza.cook_time)
-		formatted_pizza["price"] = str(pizza.price)
-		formatted_pizza["topping"] = pizza.topping
-		if pizza.customer:
-			formatted_pizza["customer_username"] = pizza.customer.username
-			formatted_pizza["customer_phone"] = pizza.customer.phone
-		else:
-			formatted_pizza["customer_username"] = ""
-			formatted_pizza["customer_phone"] = ""
+		formatted_pizza = pizza_to_dict(pizza, customer=False)
+		pizzas.append(formatted_pizza)
+
+	return HttpResponse(str(pizzas))
+
+@login_required
+def delivered_pizzas(request):
+	pizzas = list()
+	for pizza in Pizza.objects.select_related().filter(delivered=True).filter(driver=request.user.id):
+		formatted_pizza = pizza_to_dict(pizza)
 		pizzas.append(formatted_pizza)
 		
 	return HttpResponse(str(pizzas))
+
+@login_required
+def to_deliver_pizzas(request):
+	pizzas = list()
+	for pizza in Pizza.object.select_related().filter(delivered=False).filter(driver=request.user.id):
+		formatted_pizza = pizza_to_dict(pizza, customer=False)
+		pizzas.append(formatted_pizza)
+		
+	return HttpResponse(str(pizzas))
+
+def pizza_to_dict(pizza, customer=True):
+	formatted_pizza = dict()
+	formatted_pizza["cook_time"] = str(pizza.cook_time)
+	formatted_pizza["price"] = str(pizza.price)
+	formatted_pizza["topping"] = pizza.topping
+	if customer and pizza.customer:
+		formatted_pizza["customer_username"] = pizza.customer.username
+		formatted_pizza["customer_phone"] = pizza.customer.phone
+	elif customer:
+		formatted_pizza["customer_username"] = ""
+		formatted_pizza["customer_phone"] = ""
+	
+	return formatted_pizza
 
 @login_required
 def buy_pizza(request):
