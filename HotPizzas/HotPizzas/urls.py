b75857b71fb936292d12dbee8052a3096981c137
David J. Felix <felix.davidj@gmail.com>
2014-05-03 14:15:16 -0400

    Add login and log out urls

--- HotPizzas/HotPizzas/urls.py
+++ HotPizzas/HotPizzas/urls.py
@@ -9,4 +9,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    #/login/ to sign in to the application
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    #/logout/ to sign out of the application
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'login.html'}),
 )
