f7a11fd7bb40f102fa26630e424f22d1d97065e2
David J. Felix <felix.davidj@gmail.com>
2014-05-03 14:59:42 -0400

    Add a basic get function for available pizzas

--- HotPizzas/PizzaTracker/views.py
+++ HotPizzas/PizzaTracker/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from PizzaTracker.models import *
 
 def home(request):
 	return HttpResponse("Hello World")
 
 @login_required
-def driver_view(request):
-	pass
+def available_pizzas(request):
+	pizzas = [
+		{
+			"cook_time": str(pizza.cook_time),
+			"price": pizza.price,
+			"topping": pizza.topping,
+			"customer_username": pizza.customer.username,
+			"customer_phone": pizza.customer.phone_number,
+		} for pizza in Pizza.objects.select_related().filter(driver=request.user.id)
+	]
+	return HttpResponse(str(pizzas))
 
 @login_required
 def buy_pizza(request):
