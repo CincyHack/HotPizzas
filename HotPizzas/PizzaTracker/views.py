ecc24b28230c6744bfcd410d0a31b641bd79bdd6
David J. Felix <felix.davidj@gmail.com>
2014-05-04 12:45:36 -0400

    Update views.py

--- HotPizzas/PizzaTracker/views.py
+++ HotPizzas/PizzaTracker/views.py
@@ -74,9 +74,10 @@ def anonymous_pizza_browser(request):
 	if request.is_ajax() and request.method == 'POST':
 		form = LocationForm(request.POST)
 		if form.is_valid():
-			unclaimed_pizzas = Pizza.objects.select_related().filter(Q(customer__isnull=True) | Q(customer__user_id=request.user.id))
+			unclaimed_pizzas = Pizza.objects.select_related().filter(customer__isnull=True)
+			my_pizzas = Pizza.objects.select_related().filter(customer__user=request.user.id)
 			close_pizzas = list()
-			for pizza in unclaimed_pizzas:
+			for pizza in list(unclaimed_pizzas) + list(my_pizzas):
 				temp_pizza = dict()
 				temp_pizza["pizza_id"] = pizza.id
 				temp_pizza["cook_time"] = str(pizza.cook_time)
