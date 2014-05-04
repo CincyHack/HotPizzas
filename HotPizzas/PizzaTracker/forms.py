e4a4409a416858ea76d0fa364fc45a71d1eba956
David J. Felix <felix.davidj@gmail.com>
2014-05-04 12:33:43 -0400

    dont need no stinkin email

--- HotPizzas/PizzaTracker/forms.py
+++ HotPizzas/PizzaTracker/forms.py
@@ -11,12 +11,11 @@ class LocationForm(forms.Form):
 
 class UserForm(forms.ModelForm):
 	username = forms.CharField(help_text="Please enter a username.")
-	email = forms.EmailField(help_text="Please enter your email.")
 	password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password')
+		fields = ('username', 'password')
 		
 class CustomerForm(forms.ModelForm):
 	phone_number = forms.RegexField(
