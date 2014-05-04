790ec3485c53660c056113ca6696b90f125fa00b
David J. Felix <felix.davidj@gmail.com>
2014-05-04 08:26:45 -0400

    Fix import error

--- HotPizzas/PizzaTracker/forms.py
+++ HotPizzas/PizzaTracker/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from PizzaTracker.models import *
 from datetime import datetime
 
-class UserForm(forms.ModelForm):
+class UserForm(ModelForm):
 	username = forms.CharField(help_text="Please enter a username.")
 	email = forms.EmailField(help_text="Please enter your email.")
 	password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
@@ -12,7 +12,7 @@ class UserForm(forms.ModelForm):
 		model = User
 		fields = ('username', 'email', 'password')
 		
-class CustomerForm(forms.ModelForm):
+class CustomerForm(ModelForm):
 	phone_number = forms.RegexField(
 		regex=r'^\+?1?\d{9,15}$', 
 		error_message=("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -24,7 +24,7 @@ class CustomerForm(forms.ModelForm):
 		model = Customer
 		fields = ('phone_number', 'latitude', 'logitude')
 		
-class PizzaForm(forms.ModelForm):
+class PizzaForm(ModelForm):
 	cook_time = forms.DateTimeField(initial=datetime.now())
 	price = forms.DecimalField(max_digits=4, decimal_places=2)
 	topping = forms.ChoiceField(Pizza.TOPPING_CHOICES)
@@ -32,10 +32,11 @@ class PizzaForm(forms.ModelForm):
 		model = Pizza
 		fields = ('cook_time', 'price', 'topping')
 		
-class DriverForm(forms.ModelForm):
+class DriverForm(ModelForm):
 	latitude = forms.DecimalField(widget=forms.HiddenInput(), max_digits=20, decimal_places=17)
 	longitude = forms.DecimalField(widget=forms.HiddenInput(), max_digits=20, decimal_places=17)
 	
 	class Meta:
 		model = Driver
 		fields = ('latitude', 'longitude')
+
