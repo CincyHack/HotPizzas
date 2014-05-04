65fe9b8b22e5943ebbf56394ef4f67b9adc6893d
David J. Felix <felix.davidj@gmail.com>
2014-05-03 20:56:25 -0400

    Refactor to remove bug in pizza to dict and reduce

--- HotPizzas/PizzaTracker/views.py
+++ HotPizzas/PizzaTracker/views.py
@@ -5,21 +5,9 @@ from PizzaTracker.models import *
 
 @login_required
 def driver_dashboard(request):
-	pizzas_available = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=False).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza, customer=False)
-		pizzas_available.append(formatted_pizza)
-	
-	pizzas_delivered = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=True).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza)
-		pizzas_delivered.append(formatted_pizza)
-		
-	pizzas_to_deliver = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=False).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza, customer=False)
-		pizzas_to_deliver.append(formatted_pizza)
-	
+	pizzas_available = pizza_to_dict(request.user.id, delivered=False, customer=False)
+	pizzas_delivered = pizza_to_dict(request.user.id, delivered=True)
+	pizzas_to_deliver = pizza_to_dict(request.user.id, customer=False, delivered=False)
 	ctx = {'pizzas_available': pizzas_available, 'pizzas_delivered': pizzas_delivered, 'pizzas_to_deliver': pizzas_to_deliver }
 	
 	return render(request, 'driver-pizzas.html', ctx)
@@ -30,52 +18,48 @@ def home(request):
 
 @login_required
 def available_pizzas(request):
-	pizzas = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=False).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza, customer=False)
-		pizzas.append(formatted_pizza)
-
+	pizzas = pizza_to_dict(request.user.id, delivered=False, customer=False)
 	return HttpResponse(str(pizzas))
 
 @login_required
 def delivered_pizzas(request):
-	pizzas = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=True).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza)
-		pizzas.append(formatted_pizza)
-		
+	pizzas = pizza_to_dict(request.user.id, delivered=True)
 	return HttpResponse(str(pizzas))
 
 @login_required
 def to_deliver_pizzas(request):
-	pizzas = list()
-	for pizza in Pizza.objects.select_related().filter(delivered=False).filter(driver__user_id=request.user.id):
-		formatted_pizza = pizza_to_dict(pizza, customer=False)
-		pizzas.append(formatted_pizza)
-		
+	pizzas = pizza_to_dict(request.user.id, customer=False, delivered=False)
 	return HttpResponse(str(pizzas))
 
-def pizza_to_dict(pizza, customer=True):
-	formatted_pizza = dict()
-	formatted_pizza["cook_time"] = str(pizza.cook_time)
-	formatted_pizza["price"] = str(pizza.price)
-	formatted_pizza["topping"] = pizza.topping
-	if customer and pizza.customer != None:
-		formatted_pizza["customer_username"] = pizza.customer.user.username
-		formatted_pizza["customer_phone"] = pizza.customer.phone
-		formatted_pizza["customer_latitude"] = pizza.customer.latitude
-		formatted_pizza["customer_longitude"] = pizza.customer.longitude
-		if pizza.customer.user.lastname != None and pizza.customer.user.firstname != None:
-			formatted_pizza["customer_fistname"] = pizza.customer.user.firstname + pizza.customer.user.lastname
-		else:
-			formatted_pizza["customer_fullname"] = ""
-
-	elif customer:
-		formatted_pizza["customer_username"] = ""
-		formatted_pizza["customer_phone"] = ""
-		formatted_pizza["customer_fullname"] = ""
+def pizza_to_dict(user_id, customer=True, delivered=False):
+	pizzas = list()
 	
-	return formatted_pizza
+	for pizza in for pizza in Pizza.objects.select_related().filter(delivered=delivered).filter(driver__user_id=user_id):
+		formatted_pizza = dict()
+		formatted_pizza["cook_time"] = str(pizza.cook_time)
+		formatted_pizza["price"] = str(pizza.price)
+		formatted_pizza["topping"] = pizza.topping
+		if pizza.customer != None:
+			formatted_pizza["customer_username"] = pizza.customer.user.username
+			formatted_pizza["customer_phone"] = pizza.customer.phone
+			formatted_pizza["customer_latitude"] = pizza.customer.latitude
+			formatted_pizza["customer_longitude"] = pizza.customer.longitude
+			if pizza.customer.user.lastname != None and pizza.customer.user.firstname != None:
+				formatted_pizza["customer_fistname"] = pizza.customer.user.firstname + pizza.customer.user.lastname
+			else:
+				formatted_pizza["customer_fullname"] = ""
+
+		else:
+			formatted_pizza["customer_username"] = ""
+			formatted_pizza["customer_phone"] = ""
+			formatted_pizza["customer_fullname"] = ""
+	
+		if pizza.customer == None and customer == False:
+			pizzas.append(formatted_pizza)
+		elif pizza.customer != None and customer == True:
+			pizzas.append(formatted_pizza)
+			
+	return pizzas
 
 @login_required
 def buy_pizza(request):
