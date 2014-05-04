cfa44d25e5c7833ccfce7cd60ab32022812e40de
David J. Felix <felix.davidj@gmail.com>
2014-05-04 00:50:10 -0400

    Add route for customer dash

--- HotPizzas/HotPizzas/urls.py
+++ HotPizzas/HotPizzas/urls.py
@@ -4,14 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'HotPizzas.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'PizzaTracker.views.home', name='home'),
     url(r'^pizzas/available/$', 'PizzaTracker.views.available_pizzas', name='available_pizzas'),
     url(r'^pizzas/delivered/$', 'PizzaTracker.views.delivered_pizzas', name='delivered_pizzas'),
     url(r'^pizzas/to-deliver/$', 'PizzaTracker.views.to_deliver_pizzas', name='to_deliver_pizzas'),
     url(r'^driver/$', 'PizzaTracker.views.driver_dashboard', name='driver_dashboard'),
+    url(r'^customer/$', 'PizzaTracker.views.customer_dashboard', name='customer_dashboard'),
     url(r'^admin/', include(admin.site.urls)),
     #/login/ to sign in to the application
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
