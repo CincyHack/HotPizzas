268aa0b06085074a5b49820ce4af43f9db7a4d2a
Taylor Edmiston <tedmiston@gmail.com>
2014-05-04 04:09:15 -0400

    convert cook time and request time timestamps to relative times using moment.js

--- HotPizzas/PizzaTracker/views.py
+++ HotPizzas/PizzaTracker/views.py
@@ -59,11 +59,11 @@ def pizza_to_dict(user_id, customer=True, delivered=False):
 	
 	for pizza in Pizza.objects.select_related().filter(delivered=delivered).filter(driver__user_id=user_id):
 		formatted_pizza = dict()
-		formatted_pizza["cook_time"] = str(pizza.cook_time)
+		formatted_pizza["cook_time"] = '%s -0400' % (pizza.cook_time)
 		formatted_pizza["price"] = '$%.2f' % (pizza.price)
 		formatted_pizza["topping"] = pizza.get_topping_display()
 		if pizza.request_time:
-			formatted_pizza["request_time"] = str(pizza.request_time)
+			formatted_pizza["request_time"] = '%s -0400' % (pizza.request_time)
 		else:
 			formatted_pizza["request_time"] = ""
 
