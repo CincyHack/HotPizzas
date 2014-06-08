9319f9db39d8999b9bb3525913eadd2346d1b5f4
David J Felix <felix.davidj@gmail.com>
2014-06-08 14:45:36 +0000

    Migrate location of views to make room

---
+++ src/core/views/__init__.py
@@ -0,0 +1,19 @@
+#FIXME: remove import *
+from views.base import *
+
+__all__ = [
+	update_pizza,
+	customer_dashboard,
+	anonymous_pizza_browser,
+	add_pizza,
+	deliver_pizza,
+	driver_dashboard,
+	home,
+	available_pizzas,
+	delivered_pizzas,
+	to_deliver_pizzas,
+	available_pizzas_as_dict,
+	to_deliver_pizzas_as_dict,
+	pizza_to_dict,
+	buy_pizza,
+]
