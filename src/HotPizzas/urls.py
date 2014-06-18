004fb2a0b9d134800504e4ef86bbc2dd421e35e9
David J. Felix <felix.davidj@gmail.com>
2014-06-18 00:23:04 -0400

    Ditch most of the views in urls

--- src/HotPizzas/urls.py
+++ src/HotPizzas/urls.py
@@ -5,16 +5,7 @@ from core.views import PizzaListView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'core.views.home', name='home'),
-    url(r'^pizzas/available/$', 'core.views.available_pizzas', name='available_pizzas'),
-    url(r'^pizzas/delivered/$', 'core.views.delivered_pizzas', name='delivered_pizzas'),
-    url(r'^pizzas/to-deliver/$', 'core.views.to_deliver_pizzas', name='to_deliver_pizzas'),
-    url(r'^add-pizza/$', 'core.views.add_pizza', name='add_pizza'),
-    url(r'^deliver-pizza/$', 'core.views.deliver_pizza', name='deliver_pizza'),
-    url(r'^driver/$', 'core.views.driver_dashboard', name='driver_dashboard'),
-    url(r'^customer/$', 'core.views.customer_dashboard', name='customer_dashboard'),
-    url(r'^pizzas/closest/$', PizzaListView.as_view(), name='anonymous_pizza_browser'),
-    url(r'^pizza/update/$', 'core.views.update_pizza', name='update_pizza'),
+    url(r'^$', PizzaListView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     #/login/ to sign in to the application
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
