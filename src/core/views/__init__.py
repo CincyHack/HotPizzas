d328c8ffb241ab43b7468af288e5a5bb44c48a0a
David J. Felix <felix.davidj@gmail.com>
2014-06-18 00:23:47 -0400

    Remove old views from view init

--- src/core/views/__init__.py
+++ src/core/views/__init__.py
@@ -1,21 +1,5 @@
-#FIXME: remove import *
-from .base import *
 from .pizzas import PizzaListView
 
 __all__ = [
-	'update_pizza',
-	'customer_dashboard',
-	'anonymous_pizza_browser',
-	'add_pizza',
-	'deliver_pizza',
-	'driver_dashboard',
-	'home',
-	'available_pizzas',
-	'delivered_pizzas',
-	'to_deliver_pizzas',
-	'available_pizzas_as_dict',
-	'to_deliver_pizzas_as_dict',
-	'pizza_to_dict',
-	'buy_pizza',
 	'PizzaListView',
 ]
