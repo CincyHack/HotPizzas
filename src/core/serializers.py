5f4561ca59843ca1907d5eba979bf6200dd60387
David J. Felix <felix.davidj@gmail.com>
2014-06-24 22:39:01 -0400

    Update serializers.py

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -3,23 +3,30 @@ from .models import Product, ProductConfiguration, Driver, Customer
 
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+	products = serilizers.HyperlinkedRelatedField(view_name='product-detail', many=True)
+	
 	class Meta:
 		model = Customer
-		fields = ('url', 'phone_number')
+		fields = ('url', 'phone_number', 'products')
 
 class DriverSerializer(serializers.HyperlinkedModelSerializer):
+	products = serializers.HyperlinkedRelatedField(view_name='product-detail', many=True)
+	
 	class Meta:
 		model = Driver
-		fields = ('url', 'email', 'phone_number')
+		fields = ('url', 'email', 'phone_number', 'products')
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+	product_configurations = serializers.HyperlinkedRelatedField(view_name='product_configuration-details', many=True)
+	
 	class Meta:
 		model = Product
-		fields = ('url', 'cook_time', 'expiration_time', 'base_price', 'delivered', 'request_time')
+		fields = ('url', 'cook_time', 'expiration_time', 'base_price', 'delivered', 'request_time', 'product_configurations')
 
 
 class ProductConfigurationSerializer(serializers.HyperlinkedModelSerializer):
+	
 	class Meta:
 		model = ProductConfiguration
 		fields = ('url', 'description')
