fb655cc53f79858b321a2808182c54bfaa7a9ed9
David J. Felix <felix.davidj@gmail.com>
2014-07-09 19:27:16 -0400

    Update __init__.py

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,8 +1,9 @@
-from .base import Customer, Driver
+from .base import Customer, CustomerInformation, Driver
 from .product import Product, ProductType, ProductConfiguration
 
 __all__ = [
 	'Customer',
+	'CustomerInformation',
 	'Driver',
 	'Product',
 	'ProductType',
