bac7c28619d28ed2aa1a3fc2ffd16e0228fd4798
David J. Felix <felix.davidj@gmail.com>
2014-07-10 23:08:52 -0400

    Use tabs and wire LocationViewSet

--- src/HotPizzas/urls.py
+++ src/HotPizzas/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from core.views import (
-    ProductViewSet,
-    ProductTypeViewSet,
-    ProductConfigurationViewSet,
-    DriverViewSet,
-    CustomerViewSet,
-    CustomerInformationViewSet
+	ProductViewSet,
+	ProductTypeViewSet,
+	ProductConfigurationViewSet,
+	DriverViewSet,
+	CustomerViewSet,
+	CustomerInformationViewSet,
+	LocationViewSet,
 )
 from rest_framework.routers import DefaultRouter
 admin.autodiscover()
@@ -18,13 +19,14 @@ router.register(r'product_type', ProductTypeViewSet)
 router.register(r'product_configuration', ProductConfigurationViewSet)
 router.register(r'driver', DriverViewSet)
 router.register(r'customer', CustomerViewSet)
+router.register(r'location', LocationViewSet)
 
 urlpatterns = patterns('',
-    url(r'^api/', include(router.urls)),
-    url(r'^admin/', include(admin.site.urls)),
-    #/login/ to sign in to the application
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    #/logout/ to sign out of the application
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'login.html'}),
+	url(r'^api/', include(router.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	#/login/ to sign in to the application
+	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+	#/logout/ to sign out of the application
+	url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'login.html'}),
 )
 
