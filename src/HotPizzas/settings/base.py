c94cf38962f8826504f343af248137bac9d61f1f
David J. Felix <felix.davidj@gmail.com>
2014-08-17 23:32:12 -0400

    Add OAuth2Authentication

--- src/HotPizzas/settings/base.py
+++ src/HotPizzas/settings/base.py
@@ -112,6 +112,7 @@ REST_FRAMEWORK = {
 		'rest_framework.authentication.SessionAuthentication',
 		'rest_framework.authentication.BasicAuthentication',
 		'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+		'oauth2_provider.ext.rest_framework.OAuth2Authentication',
 	),
 }
 
