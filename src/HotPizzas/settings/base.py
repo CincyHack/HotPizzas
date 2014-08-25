c7a5d8391bb6b5186d4b1f7b02d20ab28f4fecf0
David J Felix <felix.davidj@gmail.com>
2014-08-25 00:02:15 +0000

    Clear out some white lines

--- src/HotPizzas/settings/base.py
+++ src/HotPizzas/settings/base.py
@@ -30,14 +30,11 @@ SECRET_KEY = '$!g0skv4ku_etamv6pxzyk7m_i)2^7$*@pcqwqqs+9mig!&q^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -67,7 +64,6 @@ WSGI_APPLICATION = 'HotPizzas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -82,21 +78,15 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL="/login/"
