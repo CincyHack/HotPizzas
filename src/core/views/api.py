1279503f25b3695ffb86bed19848bb6dded1c31f
David J. Felix <felix.davidj@gmail.com>
2014-10-10 22:45:28 -0400

    DRY-ify unique controller

--- src/core/views/api.py
+++ src/core/views/api.py
@@ -45,8 +45,7 @@ class UniqueProductViewSet(GenericViewSet):
 		return Response(serializer.data)
 
 
-	def retrieve(self, request, pk=None):
-		
+	def get_filtered_set(self, request, pk=None):
 		if pk:
 			#FIXME: handle malformed strings, excessive configurations, etc - may need to do model work
 			search_terms = pk.split("-")
@@ -61,6 +60,14 @@ class UniqueProductViewSet(GenericViewSet):
 
 			for configuration in configurations[:filter_thresh]:
 				queryset = queryset.filter(configurations__description=configuration)
+				
+			return (queryset, status)
+
+
+	def retrieve(self, request, pk=None):
+		
+		if pk:
+			(queryset, status) = self.get_filtered_set(request, pk)
 
 			serializer = self.serializer_class(queryset, many=True)
 			return Response(serializer.data, status=status)
@@ -72,20 +79,7 @@ class UniqueProductViewSet(GenericViewSet):
 	def update(self, request, pk=None):
 
 		if pk:
-			#FIXME: handle malform
-			search_terms = pk.split("-")
-			product_type = search_terms[0]
-			configurations = search_terms[1:]
-			filter_thresh = 5
-
-			# Flag for 206 partial content return. 5 is arbitrary long call
-			status = status.HTTP_200_OK if (len(configurations) < filter_thresh) else status.HTTP_206_PARTIAL_CONTENT
-
-			queryset = Product.objects.filter(product_type__name=product_type)
-
-			for configuration in configurations[:filter_thresh]:
-				queryset = queryset.filter(configurations__description=configuration)
-
+			(queryset, status) = self.get_filtered_set(request, pk)
 			queryset = queryset.first()
 			serializer = self.serializer_class(queryset)
 
