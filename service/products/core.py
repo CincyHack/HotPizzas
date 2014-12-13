20230992c086ef01856b26bcc2519bf1acf56250
David J. Felix <felix.davidj@gmail.com>
2014-12-13 00:06:48 -0500

    Add mock responses to products service

--- service/products/core.py
+++ service/products/core.py
@@ -13,10 +13,54 @@ app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route('/products', methods=('POST'))
 def view_products():
-	pass
+	return jsonify({
+		[{
+			"id": "21958c92-40c8-4ca9-b734-c8bb6b951f8d",
+			"price": 10.00,
+			"price_units": "USD",
+			"estimated_time": 10,
+			"time_units": "min",
+			"product_type": "pizza",
+			"product_configs": ["large", "pepperoni"]
+		},{
+			"id": "93b67a7c-a554-459a-b54a-4808dd466395",
+			"price": 10.00,
+			"price_units": "USD",
+			"estimated_time": 7,
+			"time_units": "min",
+			"product_type": "pizza",
+			"product_configs": ["large", "cheese"]
+		},{
+			"id": "bcfa592c-7df3-46fc-ab18-a81710960110",
+			"price": 15.00,
+			"price_units": "USD",
+			"estimated_time": 10,
+			"time_units": "min",
+			"product_type": "pizza",
+			"product_configs": ["large", "pepperoni"]
+		}]
+	})
+
 
 @app.route('/products/unique', methods('POST'))
 def view_unique_products():
-	pass
-
+	return jsonify({
+		[{
+			"id": "large-cheese",
+			"price": 10.00,
+			"price_units": "USD",
+			"estimated_time": 15,
+			"time_units": "min",
+			"product_type": "pizza",
+			"product_configs": ["large", "cheese"]
+		},{
+			"id": "large-pepperoni",
+			"price": 15.00,
+			"price_units": "USD",
+			"estimated_time": 15,
+			"time_units": "min",
+			"product_type": "pizza",
+			"product_configs": ["large", "pepperoni"]
+		}]
+	})
 
