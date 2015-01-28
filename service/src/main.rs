44ec192a1bc097c68df371adf48ebcb523e1da88
David J Felix <felix.davidj@gmail.com>
2015-01-28 13:33:55 +0000

    Begin an iron project

---
+++ service/src/main.rs
@@ -0,0 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|&: _: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).listen("localhost:3333").unwrap();
+}
